c920d13cd9c6bda9cfa0bd99fba283587277be6d
Abrdx <152388568+Abrdx@users.noreply.github.com>
2024-04-05 14:45:59 +0200

    correction

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@
 #include <GLFW/glfw3.h>
 
 #include "OPENGL/src/vendor/stb_image/stb_image.h" // bibliothèque permettant d'utiliser facilement des images pour les textures
-#include "OPENGL/src/vendor/glm/glm.hpp" // bibliothèque de maths adapté à OpenGL
-#include "OPENGL/src/vendor/glm/gtc/matrix_transform.hpp"// bibliothèque de maths adapté à OpenGL
+#include "OPENGL/src/vendor/glm/glm.hpp" // bibliothèque de maths adaptée à OpenGL
+#include "OPENGL/src/vendor/glm/gtc/matrix_transform.hpp"// bibliothèque de maths adaptée à OpenGL
 
 #include "OPENGL/src/vendor/imgui/imgui.h" // ImGui
 #include "OPENGL/src/vendor/imgui/imgui_impl_glfw_gl3.h"// ImGui
 
-// Les fichiers ci dessous sont ceux que nous avons fait, avec l'aide du tutoriel OpenGl de The cherno pour les fichier du dossier OPENGL, et inspiration  du travail de Sebastian Lague pour débuter la simulation du fluide
+// Les fichiers ci-dessous sont ceux que nous avons fait, avec l'aide du tutoriel OpenGl de The cherno pour les fichiers du dossier OPENGL, et inspiration du travail de Sebastian Lague pour débuter la simulation du fluide
 #include "physique/header/particule.h" 
 #include "physique/header/ensemble.h"
 #include "physique/header/aux.h"
@@ -40,7 +40,7 @@ using namespace std;
 
 int main(){
 
-    bool initial = true;// True tant que la définition des paramètres n'est pas fini
+    bool initial = true;// True tant que la définition des paramètres n'est pas finie
 
     int resolution_densite = 960/4; // Change le nombre de "pixel" utilisés pour afficher la densité. Non modifiable durant la simulation
 
@@ -50,15 +50,15 @@ int main(){
     bool pause=false;
     bool pause_change = false;
     float opacite = 1; // permet d'influer sur la transparence lorsque le flou est actif. 
-    bool affiche_vitesses_colorees = false; // permet de creer un gradient de couleur entre les particules lente et rapide. 
+    bool affiche_vitesses_colorees = false; // permet de créer un gradient de couleur entre les particules lentes et rapides. 
     float vitesse_caracteristique =1; // influe sur l'échelle des vitesses pour l'affichage. 
     //Rouge : v_particule >= 2*vitesse_caracteristique, magenta si v_particule=vitesse_caracteristique, bleu si v_particule=0. 
     //Augmentation/diminution linéaire des composantes rouges et bleu en fonction de la vitesse.
 
-    // variables  servant à interagir avec le fluide en imposant des forces extérieurs. Le clique gauche n'est pas consistent, il est recommendé d'utiliser "a" et "e"
+    // variables  servant à interagir avec le fluide en imposant des forces extérieures. Le clique gauche n'est pas consistent, il est recommendé d'utiliser "a" et "e"
     float sourisx = 0; // position du curseur
     float sourisy = 0;
-    bool clique_gauche=false; //Variables contenant true si la touche est pressé.
+    bool clique_gauche=false; //Variables contenant true si la touche est pressée.
     bool clique_droit=false;
     bool a_key=false;
     bool z_key=false;
@@ -71,10 +71,10 @@ int main(){
     float puissance_action_autour_curseur = 100;
     int sens_action_clique_gauche=1; 
     
-    // Paramètres Partagés, modifiable durant la simulation
+    // Paramètres partagés, modifiables durant la simulation
 
     float dt =0.003; // Partagé pour que la simutation reste cohérente, le temps s'écoule pour tout le monde pareil(dans notre modeste modèle)
-    float rayon_influence =0.1; // Partagé pour simplifier les interaction entre particules différentes et simplifier lors de l'utilisation des listes d'indices
+    float rayon_influence =0.1; // Partagé pour simplifier les interactions entre particules différentes et simplifier lors de l'utilisation des listes d'indices
     float vx_boite = 0; // vitesse des bords haut et bas de la boite
     float vy_boite = 0; // vitesse des bords gauch et droit de la boite
 
@@ -83,38 +83,38 @@ int main(){
     // Paramètres pour le premier fluide
 
     unsigned int nombre_de_particules =1000; // Non modifiable durant la simulation
-    int nb =nombre_de_particules; // Pour l'initialisation car je n'ai pas trouvé d'équivalent a ImGui::InputInt pour un unsigned int.
+    int nb =nombre_de_particules; // Pour l'initialisation car je n'ai pas trouvé d'équivalent à ImGui::InputInt pour un unsigned int.
 
     // Paramètres modifiables durant la simulation
-    float rayon_collision = 0.01; // rayon utilisé pour les collisions avec les parois et les objets. N'intervient pas dans les interactions particulaire. Rayon utilisé pour l'affichage (sans flou)
-    float g = 10; // contole la norme de la gravitée. A utiliser pour faire varier les densitées des fluides (A justifier, cf : )
-    float masse = 1; // Proportionnel à la masse ajouté par particule, répartie dans le disque de rayon rayon_influence. 
+    float rayon_collision = 0.01; // rayon utilisé pour les collisions avec les parois et les objets. N'intervient pas dans les interactions particulaires. Rayon utilisé pour l'affichage (sans flou)
+    float g = 10; // contrôle la norme de la gravité. À utiliser pour faire varier les densités des fluides (cf rapport )
+    float masse = 1; // Proportionnel à la masse ajoutée par particule, répartie dans le disque de rayon rayon_influence. 
     float multiplicateur_pression = 100; // Force de l'interaction de répulsion/attraction entre 2 particules de même type
-    float multiplicateur_pression_proche = 1000; // Force de l'interaction de répulsion entre 2 particules de même type a courte distance. Aide à la génération de pseudo-tension de surface
-    float densite_visee = 1000; // Densitée à laquelle il n'y à plus de force de pression. (peut encore y avoir de la force de pression proche)
+    float multiplicateur_pression_proche = 1000; // Force de l'interaction de répulsion entre 2 particules de même type à courte distance. Aide à la génération de pseudo-tensions de surface
+    float densite_visee = 1000; // Densité à laquelle il n'y a plus de forces de pression. (il peut encore y avoir de la force de pression proche)
     float coeff_amorti = 0.9; // coefficient multiplicatif lors du rebond sur un mur
     float coeff_viscosite = 0.1; // coefficient multiplicatif de la force visqueuse entre 2 particules de même type
-    float coeff_adherence = 0.005; // Coefficient d'adhérence à la paroi
-    int texture_debut = 0; // utilisation de texture pour le fluide, celles qui sont bind entre texturedebut et texturefin. 0 correspond a aucune texture.
+    float coeff_adherence = 0.005; // coefficient d'adhérence à la paroi
+    int texture_debut = 0; // utilisation de texture pour le fluide, celles qui sont bind entre texturedebut et texturefin. 0 correspond à aucune texture.
     int texture_fin = 0;
 
-    // Paramètres pour le second fluiden il sera définit si nombre_de_particule2>0
+    // Paramètres pour le second fluide; il sera défini si nombre_de_particule2>0
 
     unsigned int nombre_de_particules2 =0;// Non modifiable durant la simulation
-    int nb2 =nombre_de_particules2; // Pour l'initialisation car je n'ai pas trouvé d'équivalent a ImGui::InputInt pour un unsigned int.
+    int nb2 =nombre_de_particules2; // Pour l'initialisation car je n'ai pas trouvé d'équivalent à ImGui::InputInt pour un unsigned int.
     
     
     // Paramètres modifiables durant la simulation
-    float rayon_collision2 = 0.01; // rayon utilisé pour les collisions avec les parois et les objets. N'intervient pas dans les interactions particulaire. Rayon utilisé pour l'affichage (sans flou)
-    float g2 = 10; // contole la norme de la gravitée. A utiliser pour faire varier les densitées des fluides (A justifier, cf : )
-    float masse2 = 1; // Proportionnel à la masse ajouté par particule, répartie dans le disque de rayon rayon_influence. 
+    float rayon_collision2 = 0.01; // rayon utilisé pour les collisions avec les parois et les objets. N'intervient pas dans les interactions particulaires. Rayon utilisé pour l'affichage (sans flou)
+    float g2 = 10; // contrôle la norme de la gravité. À utiliser pour faire varier les densités des fluides ( cf rapport )
+    float masse2 = 1; // Proportionnel à la masse ajoutée par particule, répartie dans le disque de rayon rayon_influence. 
     float multiplicateur_pression2 = 100; // Force de l'interaction de répulsion/attraction entre 2 particules de même type
-    float multiplicateur_pression_proche2 = 1000; // Force de l'interaction de répulsion entre 2 particules de même type a courte distance. Aide à la génération de pseudo-tension de surface
-    float densite_visee2 = 1000; // Densitée à laquelle il n'y à plus de force de pression. (peut encore y avoir de la force de pression proche)
+    float multiplicateur_pression_proche2 = 1000; // Force de l'interaction de répulsion entre 2 particules de même type à courte distance. Aide à la génération de pseudo-tensions de surface
+    float densite_visee2 = 1000; // Densité à laquelle il n'y a plus de force de pression. (il peut encore y avoir de la force de pression proche)
     float coeff_amorti2 = 0.9; // coefficient multiplicatif lors du rebond sur un mur
     float coeff_viscosite2 = 0.1; // coefficient multiplicatif de la force visqueuse entre 2 particules de même type
-    float coeff_adherence2 = 0.005; // Coefficient d'adhérence à la paroi
-    int texture_debut2 = 0; // utilisation de texture pour le fluide, celles qui sont bind entre texturedebut et texturefin. 0 correspond a aucune texture.
+    float coeff_adherence2 = 0.005; // coefficient d'adhérence à la paroi
+    int texture_debut2 = 0; // utilisation de texture pour le fluide, celles qui sont bind entre texturedebut et texturefin. 0 correspond à aucune texture.
     int texture_fin2 = 0;
     
     //Paramètres d'interaction entre les 2 fluides
@@ -181,24 +181,24 @@ int main(){
     }
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    // Active la possibilité de transparence et défini la fonction de transparence
+    // Active la possibilité de transparence et définit la fonction de transparence
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA));
 
     
     int TexID[32]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};//utile pour avoir plusieurs texture par fluide
-    // matrice de transformation, mais peu utilisé dans notre cas
+    // matrice de transformation, mais peu utilisée dans notre cas
     glm::mat4 proj = glm::ortho(-1.0,1.0,-1.0,1.0,-1.0,1.0);
     glm::mat4 view=glm::translate(glm::mat4(1.0),glm::vec3(0,0,0));
     glm::mat4 model=glm::translate(glm::mat4(1.0),glm::vec3(0,0,0));
     glm::mat4 mvp = proj * view * model;
 
-    // Initialisation des objets nécessaire pour l'affichage
+    // Initialisation des objets nécessaires pour l'affichage
     Shader densite_shader("OPENGL/res/shaders/densite.shader");
     Shader fluide_shader("OPENGL/res/shaders/fluide.shader");
     Renderer renderer;
     fluide_shader.Bind();
-    // importation des textures et bind de celles ci.
+    // importation des textures et bind de celles-ci.
     Texture t1("OPENGL/res/textures/eau.png");Texture t2("OPENGL/res/textures/eau1.png");
     Texture t3("OPENGL/res/textures/eau2.png");Texture t4("OPENGL/res/textures/eau3.png");
     Texture t5("OPENGL/res/textures/huile.png");Texture t6("OPENGL/res/textures/huile2.png");
@@ -224,7 +224,7 @@ int main(){
 
 
         ImGui_ImplGlfwGL3_NewFrame();        
-        // actualise les constante à l'aide d'ImGui
+        // actualise les constantes à l'aide d'ImGui
         
         fenetre_principale("principale", &sourisx, &sourisy, &clique_gauche, &clique_droit, &a_key, &z_key, &e_key, &q_key, &s_key, &d_key, &dt,
  &rayon_influence, &vx_boite, &vy_boite, &rayon_action_autour_curseur, &puissance_action_autour_curseur, &sens_action_clique_gauche,
@@ -239,7 +239,7 @@ int main(){
         }
 
         
-        //Fenetre ImGui permettant d'initialiser les nombres de particules et de valider
+        //Fenêtre ImGui permettant d'initialiser les nombres de particules et de valider
         ImGui::Begin("Validation des conditions initiales");
         {
             ImGui::InputInt("Nombre de particules 1",&nb);
@@ -259,12 +259,12 @@ int main(){
         GLCall(glfwPollEvents());
     }
 
-     // Initialiqation des deux liquides
+     // Initialisation des deux liquides
 
     Ensemble fluide = Ensemble(nombre_de_particules,rayon_collision);
     Ensemble fluide2 = Ensemble(nombre_de_particules2,rayon_collision);
     
-    //Tableau pour tracer tous les point en un seul "draw"
+    //Tableau pour tracer tous les points en un seul "draw"
 
     unsigned int* indices_fluide = (unsigned int*)malloc(nombre_de_particules*6*sizeof(unsigned int));
     unsigned int* indices_fluide2 = (unsigned int*)malloc(nombre_de_particules2*6*sizeof(unsigned int));
@@ -296,7 +296,7 @@ int main(){
         renderer.Clear();
 
         ImGui_ImplGlfwGL3_NewFrame();
-        // actualise les constante à l'aide d'ImGui
+        // actualise les constantes à l'aide d'ImGui
         fenetre_principale("dodo", &sourisx, &sourisy, &clique_gauche, &clique_droit, &a_key, &z_key, &e_key, &q_key, &s_key, &d_key, &dt,
  &rayon_influence, &vx_boite, &vy_boite, &rayon_action_autour_curseur, &puissance_action_autour_curseur, &sens_action_clique_gauche,
  &affiche_densite, &flou, &pause_change, &pause, &opacite, &affiche_vitesses_colorees, &vitesse_caracteristique, &logdt, &logpacg, &logvc);
@@ -309,7 +309,7 @@ int main(){
             fenetre_interaction( "mélange", &densite_visee_melange, &pression_melange,  &logpmel, &logppmel, &pression_proche_melange, &viscosite_melange, &logviscmel);
         }
 
-        // calcul et affichage de la densitée si l'option est activée
+        // calcul et affichage de la densité si l'option est activée
         if (affiche_densite){
 
             position_couleur_densite(&fluide,&fluide2,info_densite,densite_visee,resolution_densite);
@@ -327,7 +327,7 @@ int main(){
    
         }
         
-        // rempli les informations pour l'affichages des particules, Par point : xpoint,ypoint,xparticule,yparticule,R,G,B,A,TexCoordx,TexCoordy,TexID,rayon_collision²,rayon_influence²
+        // remplit les informations pour l'affichage des particules, par point : xpoint,ypoint,xparticule,yparticule,R,G,B,A,TexCoordx,TexCoordy,TexID,rayon_collision²,rayon_influence²
         fluide.rempli_info_point(info_points,vitesse_caracteristique,opacite,affiche_vitesses_colorees, texture_debut,texture_fin);
         fluide2.rempli_info_point(info_points2,vitesse_caracteristique,opacite,affiche_vitesses_colorees,texture_debut2,texture_fin2);
 
